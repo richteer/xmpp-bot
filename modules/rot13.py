fde1b3f0febf1d1d6b7680a5c7d2651db441baf9
richteer <richteer@clarkson.edu>
2014-08-13 13:47:14 -0400

    Make rot13 a one-liner

--- modules/rot13.py
+++ modules/rot13.py
@@ -1,13 +1,7 @@
 
 def rot13(string):
-	result = ""
+	return "".join([chr((((ord(c) - ord('a')) + 13 ) % 26) + ord('a')) for c in string])
 	
-	for v in string:
-		c = ord(v) - ord('a')
-		c = (c + 13) % 26
-		result += chr(c + ord('a'))
-	return result
-
 def rot13_handler(string):
 	if string == None:
 		return None
