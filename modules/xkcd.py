2778892faf6d47a7021fe70ea168ab9f715739f6
richteer <richteer@clarkson.edu>
2014-08-13 13:47:52 -0400

    XKCD now can retreive today's with !xkcd today

--- modules/xkcd.py
+++ modules/xkcd.py
@@ -3,7 +3,15 @@ import requests
 def handle_xkcd(string):
 	if string == None:
 		return None
-	
+
+	if string == "today":
+		r = requests.get("http://xkcd.com/info.0.json")
+		if r.json:
+			return 'xkcd {0} - "{1}": {2}'.format(string, r.json["title"], r.json["img"])
+		else:
+			return "Could not find that one, does it even exist?"
+
+
 	try:
 		int(string)
 	except:
